5c7c60a67e058845e23d5baae020a02bac36071b
David Felipe <felipe30.10@outlook.com>
2023-03-10 21:27:22 -0300

    add data

---
+++ src/service/data.js
@@ -0,0 +1,170 @@
+export const data = [
+    {
+        title: 'Educação infantil',
+        description: 'Nossa proposta pedagógica é formulada considerando a criança como um ser em contrução, mas que já possui inúmeros conhecimentos e estes devem ser ampliados e articulados a uma realidade concreta. Neste sentido, devem ser trabalhados temas que envolvam e auxiliem:',
+        cards: [
+            {
+                title: 'Gestão de emoções'
+            },
+            {
+                title: 'Hábitos higiênico'
+            },
+            {
+                title: 'Preservação da natureza'
+            }
+        ],
+        working: 'Deve-se entender que, para as crianças de 0 (zero) a 5 (cinco) anos, independentemente das diferentes condições no espaço escolar, as relações sociais requerem a atenção intensiva dos profissionais da educação, durante o tempo e o momento de desenvolvimento das atividades que lhes são peculiares: este é o tempo em que a curiosidade deve ser estimulada, a partir da brincadeira orientada pelos profissionais da educação.',
+        objectives: `
+            A Educação Infantil, cuja finalidade primordial é o desenvolvimento integral da 
+            criança até cinco anos de idade, nos aspectos físico, psicológico, intelectual e 
+            social, complementando a ação da família e da comunidade, tem como objetivos:
+
+            <br/>
+            <br/>
+            <span className="flex flex-col gap-3 ">
+                <p>
+                    a) Estabelecer vínculos afetivos, ampliando suas possibilidades de comunicação e interação social;
+                </p>
+                <p>
+                    b) Respeitar a diversidade e desenvolver atitudes de ajuda e colaboração;
+                </p>
+                <p>
+                    c) Incentivar a autonomia nas atividades cotidianas, assim como nas descobertas e aprendizagens;
+                </p>
+                <p>
+                    d) Observar e explorar o ambiente com atitude de curiosidade;
+                </p>
+                <p>
+                    e) Desenvolver suas expressões e habilidades de diferentes formas, estimulando suas áreas emocional, física, intelectual, e social;
+                </p>
+                <p>
+                    f) Brincar, expressando emoções, sentimentos, pensamentos, desejos e necessidades;
+                </p>
+                <p>
+                    g) Utilizar as diferentes linguagens de forma a compreender e ser compreendido, expressar suas idéias, sentimentos, necessidades e desejos;
+                </p>
+                <p>
+                    h) Conhecer algumas manifestações culturais, de interesse, respeito e participação, valorizando a diversidade.
+                </p>
+            
+            </span>
+            <br/>
+            Visamos formar cidadãos ativos, críticos e conscientes sobre si e sobre a realidade que os cerca, 
+            além de incentivar o desenvolvimento humano em sentido pleno. A aplicabilidade desta proposta se faz 
+            extremamente necessária desde as mais iniciais fases da educação e deve ser mantida até as fases finais, 
+            criando um conjunto coerente e focado num único objetivo: formação de seres humanos que transformem o mundo.
+        `
+    },
+    {
+        title: 'Jardim I',
+        description: 'Nossa proposta pedagógica é formulada considerando a criança como um ser em contrução, mas que já possui inúmeros conhecimentos e estes devem ser ampliados e articulados a uma realidade concreta. Neste sentido, devem ser trabalhados temas que envolvam e auxiliem:',
+        cards: [
+            {
+                title: 'Gestão de emoções'
+            },
+            {
+                title: 'Hábitos higiênico'
+            },
+            {
+                title: 'Preservação da natureza'
+            }
+        ],
+        working: 'Deve-se entender que, para as crianças de 0 (zero) a 5 (cinco) anos, independentemente das diferentes condições no espaço escolar, as relações sociais requerem a atenção intensiva dos profissionais da educação, durante o tempo e o momento de desenvolvimento das atividades que lhes são peculiares: este é o tempo em que a curiosidade deve ser estimulada, a partir da brincadeira orientada pelos profissionais da educação.',
+        objectives: `
+            A Educação Infantil, cuja finalidade primordial é o desenvolvimento integral da 
+            criança até cinco anos de idade, nos aspectos físico, psicológico, intelectual e 
+            social, complementando a ação da família e da comunidade, tem como objetivos:
+
+            <br/>
+            <br/>
+            <span className="flex flex-col gap-3 ">
+                <p>
+                    a) Estabelecer vínculos afetivos, ampliando suas possibilidades de comunicação e interação social;
+                </p>
+                <p>
+                    b) Respeitar a diversidade e desenvolver atitudes de ajuda e colaboração;
+                </p>
+                <p>
+                    c) Incentivar a autonomia nas atividades cotidianas, assim como nas descobertas e aprendizagens;
+                </p>
+                <p>
+                    d) Observar e explorar o ambiente com atitude de curiosidade;
+                </p>
+                <p>
+                    e) Desenvolver suas expressões e habilidades de diferentes formas, estimulando suas áreas emocional, física, intelectual, e social;
+                </p>
+                <p>
+                    f) Brincar, expressando emoções, sentimentos, pensamentos, desejos e necessidades;
+                </p>
+                <p>
+                    g) Utilizar as diferentes linguagens de forma a compreender e ser compreendido, expressar suas idéias, sentimentos, necessidades e desejos;
+                </p>
+                <p>
+                    h) Conhecer algumas manifestações culturais, de interesse, respeito e participação, valorizando a diversidade.
+                </p>
+            
+            </span>
+            <br/>
+            Visamos formar cidadãos ativos, críticos e conscientes sobre si e sobre a realidade que os cerca, 
+            além de incentivar o desenvolvimento humano em sentido pleno. A aplicabilidade desta proposta se faz 
+            extremamente necessária desde as mais iniciais fases da educação e deve ser mantida até as fases finais, 
+            criando um conjunto coerente e focado num único objetivo: formação de seres humanos que transformem o mundo.
+        `
+    },
+    {
+        title: 'Jardim II',
+        description: 'Nossa proposta pedagógica é formulada considerando a criança como um ser em contrução, mas que já possui inúmeros conhecimentos e estes devem ser ampliados e articulados a uma realidade concreta. Neste sentido, devem ser trabalhados temas que envolvam e auxiliem:',
+        cards: [
+            {
+                title: 'Gestão de emoções'
+            },
+            {
+                title: 'Hábitos higiênico'
+            },
+            {
+                title: 'Preservação da natureza'
+            }
+        ],
+        working: 'Deve-se entender que, para as crianças de 0 (zero) a 5 (cinco) anos, independentemente das diferentes condições no espaço escolar, as relações sociais requerem a atenção intensiva dos profissionais da educação, durante o tempo e o momento de desenvolvimento das atividades que lhes são peculiares: este é o tempo em que a curiosidade deve ser estimulada, a partir da brincadeira orientada pelos profissionais da educação.',
+        objectives: `
+            A Educação Infantil, cuja finalidade primordial é o desenvolvimento integral da 
+            criança até cinco anos de idade, nos aspectos físico, psicológico, intelectual e 
+            social, complementando a ação da família e da comunidade, tem como objetivos:
+
+            <br/>
+            <br/>
+            <span className="flex flex-col gap-3 ">
+                <p>
+                    a) Estabelecer vínculos afetivos, ampliando suas possibilidades de comunicação e interação social;
+                </p>
+                <p>
+                    b) Respeitar a diversidade e desenvolver atitudes de ajuda e colaboração;
+                </p>
+                <p>
+                    c) Incentivar a autonomia nas atividades cotidianas, assim como nas descobertas e aprendizagens;
+                </p>
+                <p>
+                    d) Observar e explorar o ambiente com atitude de curiosidade;
+                </p>
+                <p>
+                    e) Desenvolver suas expressões e habilidades de diferentes formas, estimulando suas áreas emocional, física, intelectual, e social;
+                </p>
+                <p>
+                    f) Brincar, expressando emoções, sentimentos, pensamentos, desejos e necessidades;
+                </p>
+                <p>
+                    g) Utilizar as diferentes linguagens de forma a compreender e ser compreendido, expressar suas idéias, sentimentos, necessidades e desejos;
+                </p>
+                <p>
+                    h) Conhecer algumas manifestações culturais, de interesse, respeito e participação, valorizando a diversidade.
+                </p>
+            
+            </span>
+            <br/>
+            Visamos formar cidadãos ativos, críticos e conscientes sobre si e sobre a realidade que os cerca, 
+            além de incentivar o desenvolvimento humano em sentido pleno. A aplicabilidade desta proposta se faz 
+            extremamente necessária desde as mais iniciais fases da educação e deve ser mantida até as fases finais, 
+            criando um conjunto coerente e focado num único objetivo: formação de seres humanos que transformem o mundo.
+        `
+    }
+];
